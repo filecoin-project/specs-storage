00a53a610bf94ace98685b58df5cffa9598fcdbb
zenground0 <ZenGround0@users.noreply.github.com>
2021-12-15 12:05:10 -0500

    update to actors v7

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/filecoin-project/specs-storage
 go 1.13
 
 require (
-	github.com/filecoin-project/go-state-types v0.0.0-20200904021452-1883f36ca2f4
-	github.com/filecoin-project/specs-actors v0.9.4
+	github.com/filecoin-project/go-state-types v0.1.1
+	github.com/filecoin-project/specs-actors/v7 v7.0.0-20211215164317-6fae51031cbe
 	github.com/ipfs/go-cid v0.0.7
 	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
 	golang.org/x/sys v0.0.0-20190826190057-c7b8b68b1456 // indirect
--- storage/storage.go
+++ storage/storage.go
@@ -7,7 +7,7 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
-	proof "github.com/filecoin-project/specs-actors/actors/runtime/proof"
+	proof "github.com/filecoin-project/specs-actors/v7/actors/runtime/proof"
 )
 
 type Data = io.Reader
@@ -26,7 +26,7 @@ type Storage interface {
 }
 
 type Prover interface {
-	GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) ([]proof.PoStProof, error)
+	GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness, currEpoch abi.ChainEpoch) ([]proof.PoStProof, error)
 	GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) (proof []proof.PoStProof, skipped []abi.SectorID, err error)
 }
 
